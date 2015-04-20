ccaf8d7efd400bd6992e048041a90d773f8b1076
Avinash Gosavi <aag1091@gmail.com>
2015-04-20 02:56:42 -0400

    Added processed file for salary data

--- get_data.rb
+++ get_data.rb
@@ -3,12 +3,18 @@ require 'csv'
 
 salary_data = CSV.read('salary_data.csv')
 
-salary_data.each do |sd|
+teachers = []
 
+salary_data.each do |sd|
+  teacher = {}
+  puts "-"*25
   mechanize = Mechanize.new
 
   first_name = sd[1]
   last_name = sd[0]
+
+  teacher[:first_name] = first_name
+  teacher[:last_name] = last_name
 
   page = mechanize.get('https://www.odu.edu/directory?F_NAME='+first_name+'&L_NAME='+last_name+'&SEARCH_IND=E')
 
@@ -17,19 +23,34 @@ salary_data.each do |sd|
   link = ""
   page.search("table.bordertable tr:nth-child(3) td:first a").each do |a|
     link = a['href'] 
-    puts a['href']
-    puts a.text.strip
+    # puts a['href']
+    puts "Name - " + a.text.strip
   end
 
   page = mechanize.get('https://www.odu.edu'+link)
 
   # puts page.inspect
 
-  department = ""
-  page.search("section.alpha ul.left_column li:nth-child(3)").each do |a|
-    department = a.text.strip
-    puts department
+  img_url = ""
+  page.search("section.alpha ul.left_column li:nth-child(1) img").each do |img|
+    img_url = img['src']
+    puts "Image Url - " + img_url
   end
+
+  department = ""
+  if img_url == ""
+    page.search("section.alpha ul.left_column li:nth-child(3)").each do |a|
+      department = a.text.strip
+      puts "Department - " + department
+    end
+  else
+    page.search("section.alpha ul.left_column li:nth-child(4)").each do |a|
+      department = a.text.strip
+      puts "Department - " + department
+    end
+  end
+
+  teacher[:department] = department
 
   education = []
   page.search(".tab-content ul.ul_in_tab li.fas_education dl").each do |a|
@@ -40,21 +61,40 @@ salary_data.each do |sd|
     end
     a.search('dd:first').each do |a|
       a.search('strong').remove
-      major = a.text.strip
+      major = a.text
     end
     a.search('dd:last').each do |a|
       a.search('strong').remove
-      degree = a.text.strip
+      degree = a.text
     end
     edu = {
-      'university' => university.strip,
-      'year' => year.strip,
-      'major' => major,
-      'degree' => degree,
+      'university' => (university || "").strip,
+      'year' => (year || "").strip,
+      'major' => (major || "").strip,
+      'degree' => (degree || "").strip,
     }
     education << edu
   end
 
-  puts education.inspect
+  puts "education"
+  puts education.first.inspect
 
+  if education.any?
+    teacher[:university] = education.first['university']
+    teacher[:year] = education.first['year']
+    teacher[:major] = education.first['major']
+    teacher[:degree] = education.first['degree']
+  end
+
+  teachers << teacher
 end
+
+puts teachers.inspect
+
+CSV.open('processed_salary_data.csv', 'w') do |csv_object|
+  csv_object << teachers.first.keys
+  teachers.each do |row_array|
+    csv_object << row_array.values
+  end
+end
+
